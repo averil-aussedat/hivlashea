ededa3ed1ab4b3f0f24dcb7467ace8d61585bcf7
averil-prost <averilprost33@gmail.com>
2022-08-06 13:46:15 +0200

    diag

--- diag_sheath.py
+++ diag_sheath.py
@@ -24,23 +24,30 @@ plt.rcParams['font.family'] = 'monospace' # fixed-case font *-*
 loadrep = "../data/"
 saverep_img = "../images/"
 saverep_mp4 = "../videos/"
-yamlfilename = "test_2sp.yaml"
+# yamlfilename = "test_2sp.yaml"
+yamlfilename = "Badsi_2022.yaml"
+# yamlfilename = "badsiBerthonCrestetto2014.yml"
 
 informations_on_graphs = True # add parameters on graphs. Keep true please...
 
-plot_fi = True # density of ions 
-plot_fe = True # density of electrons
-plot_EE = True # electric field
+plot_fi  = True # density of ions 
+plot_fe  = True # density of electrons
+plot_EE  = True # electric field
+plot_rho = True # \int_v f_i - f_e dv
 
-video_fi = True # density of ions 
-video_fe = True # density of electrons
-video_EE = True # electric field
+video_fi  = False # density of ions 
+video_fe  = False # density of electrons
+video_EE  = True # electric field
+video_rho = True # \int_v f_i - f_e dv
 
 thefiles = listdir (loadrep)
-times = [] # strong assumption : E, fe an fi are all plotted on same times
+times = [] # strong assumption : all data are all plotted on same times
 
 show_graphs = False # only for graphs, not videos
 save_graphs = True # only for graphs, not videos
+
+factor = 100.0 # 1.0 = real time, more = slow motion, less = accelerated
+cmap="gnuplot"
 
 #################################
 # Loading data
@@ -108,42 +115,61 @@ if (informations_on_graphs):
     Infos = ""
     # unstructered information
     Infos += "SL for Vlasov-Poisson, " + datetime.now().strftime("%d-%m-%Y %Hh%M") + "\n"
-    Infos += "T=%6.3f, %d time iterations (dt=%6.3f)\n" % (dt*num_iteration, num_iteration, dt)
+    Infos += "T=%6.3f, %d time iterations (dt=%2.2e)\n" % (dt*num_iteration, num_iteration, dt)
     Infos += "lambda = %6.3f, nu = %6.3f\n" % (llambda, nu)
     # mesh infos
     for mesh in [meshx, meshve, meshvi]:
-        Infos += "%8s : %4d points in [%7.3f, %7.3f]\n" % (mesh["name"],mesh["N"]+1,mesh["min"],mesh["max"])
+        Infos += "%8s : %4d points in [%7.2f, %7.2f]\n" % (mesh["name"],mesh["N"]+1,mesh["min"],mesh["max"])
     # advection infos
     for adv in [adv_x, adv_ve, adv_vi]:
         Infos += "%8s : v = %7.2f, d = %2d, kb = %2d\n" % (adv["name"], adv["v"], adv["d"], adv["kb"])
 
-### Electric fields E
-if (plot_EE or video_EE or video_fe or video_fi): # get times.
-    # first line : simulation time
-    # all the others : x_i E_i
-    Efields = []; Emin=1e6; Emax=-1e6; E_its = []
-    Efieldsnames = [filename for filename in thefiles if re.match("E[0-9]*\.dat", filename)] 
-    Efieldsnames = np.sort(Efieldsnames)
-    if (plot_EE and not video_EE):
-        Efieldsnames = [Efieldsnames[0],Efieldsnames[-1]]
-    print("Reading electric field files%s\n" % ("" if (plot_EE or video_EE) else" (just for times)"), Efieldsnames)
-    for Efieldname in Efieldsnames:
-        Efile = open(loadrep+Efieldname, "r")
-        if (plot_EE or video_EE):
-            Edata = Efile.readlines ()
-            times.append (float(Edata.pop(0).replace('\n','')))
-            Edata = [[float(x.replace('\n','')) for x in line.split(' ')] for line in Edata]
-            Efields.append ([ee[1] for ee in Edata]) 
-            xxE = [ee[0] for ee in Edata]
-            E_its.append(int(Efieldname[-len("0000.dat"):-len(".dat")])) 
-            Emin = np.min([Emin,np.min(Efields[-1])])
-            Emax = np.max([Emax,np.max(Efields[-1])])
-        else:
-            times.append (float(Efile.readline().replace('\n',''))) # just read first line
-        Efile.close ()
+Efields = []; Emin=1e6; Emax=-1e-6; E_its=[]; 
+rhos = []; rhomin=1e6; rhomax=-1e-6; rho_its=[]; 
+fes=[]; fe_its = []; mine=1.0; maxe=-1.0
+fis=[]; fi_its = []; mini=1.0; maxi=-1.0
 
-fis=[]; fes=[] # values 
-fi_its = []; fe_its = [] # number of iterations
+### Electric fields E + we do it to get times and xx
+# first line : simulation time
+# all the others : x_i E_i
+Efieldsnames = [filename for filename in thefiles if re.match("E[0-9]*\.dat", filename)] 
+Efieldsnames = np.sort(Efieldsnames)
+if (plot_EE and not video_EE):
+    Efieldsnames = [Efieldsnames[0],Efieldsnames[-1]]
+print("Reading electric field files%s\n" % ("" if (plot_EE or video_EE) else" (just for times)"), Efieldsnames)
+for Efieldname in Efieldsnames:
+    Efile = open(loadrep+Efieldname, "r")
+    if (plot_EE or video_EE):
+        Edata = Efile.readlines ()
+        times.append (float(Edata.pop(0).replace('\n','')))
+        Edata = [[float(x.replace('\n','')) for x in line.split(' ')] for line in Edata]
+        Efields.append ([ee[1] for ee in Edata]) 
+        E_its.append(int(Efieldname[-len("000000.dat"):-len(".dat")])) 
+        Emin = np.min([Emin,np.min(Efields[-1])])
+        Emax = np.max([Emax,np.max(Efields[-1])])
+    else:
+        times.append (float(Efile.readline().replace('\n',''))) # just read first line
+    Efile.close ()
+
+### rho
+if (plot_rho or video_rho):
+    # first line : simulation time
+    # all the others : x_i rho_i
+    rhosnames = [filename for filename in thefiles if re.match("rho[0-9]*\.dat", filename)] 
+    rhosnames = np.sort(rhosnames)
+    if (plot_rho and not video_rho):
+        rhosnames = [rhosnames[0],rhosnames[-1]]
+    print("Reading rho files\n", rhosnames)
+    for rhoname in rhosnames:
+        rhofile = open(loadrep+rhoname, "r")
+        rhodata = rhofile.readlines ()
+        rhodata.pop(0) # simu time
+        rhodata = [[float(x.replace('\n','')) for x in line.split(' ')] for line in rhodata]
+        rhos.append ([ee[1] for ee in rhodata]) 
+        rho_its.append(int(rhoname[-len("000000.dat"):-len(".dat")])) 
+        rhomin = np.min([rhomin,np.min(rhos[-1])])
+        rhomax = np.max([rhomax,np.max(rhos[-1])])
+        rhofile.close ()
 
 # Densities
 for (plot,vid,tag,ffs,its) in zip([plot_fe,plot_fi],[video_fe,video_fi],["fe","fi"],[fes,fis],[fe_its,fi_its]):
@@ -156,19 +182,42 @@ for (plot,vid,tag,ffs,its) in zip([plot_fe,plot_fi],[video_fe,video_fi],["fe","f
         for fname in fsnames:
             fdict = h5py.File(loadrep+fname,'r')
             ffs.append(np.transpose(fdict["values"]))
-            its.append(int(fname[-len("0000-values.h5"):-len("-values.h5")])) 
+            its.append(int(fname[-len("000000-values.h5"):-len("-values.h5")])) 
 
-maxe = max([np.max(ff) for ff in fes])
-maxi = max([np.max(ff) for ff in fis])
+if (plot_fe or video_fe):
+    maxe = max([np.max(ff) for ff in fes[-min([20,len(fes)]):]])
+    mine = min([np.min(ff) for ff in fes[-min([20,len(fes)]):]])
+    if (maxe <= 0):
+        print("WARNING : max f_{e} <=0.0).")
+        maxe=0.5
+if (plot_fi or video_fi):
+    maxi = max([np.max(ff) for ff in fis[-min([20,len(fis)]):]])
+    mini = min([np.min(ff) for ff in fis[-min([20,len(fis)]):]])
+    if (maxi <= 0):
+        print("WARNING : max f_{i} <=0.0).")
+        maxi=0.5
 
-if (maxe<=0.0 or maxi<=0):
-    print("WARNING : one of the two species is absent (max f_{i/e} <=0.0).")
-    maxe=max([maxe,0.5]); maxi=max([maxi,0.5])
+#################################
+# Verifications
+#################################
+
+EE = Efields[-1]
+rr = rhos[-1]
+dx = (meshx["max"] - meshx["min"])/meshx["N"]
+# for i in range(1,meshx["N"]):
+#     print("err : ", llambda**2 * (EE[i] - EE[i-1])/dx - rr[i])
+
+err = max([llambda**2 * (EE[i] - EE[i-1])/dx - rr[i] for i in range(1,meshx["N"])])
+print("err : ", err)
+
+print("EE : ", EE)
+print("rho : ", rr)
 
 #################################
 # Plotting figures
 #################################
 
+xx = np.linspace (meshx["min"],meshx["max"],meshx["N"]+1)
 feextent = [meshx["min"],meshx["max"],meshve["min"],meshve["max"]] # left, right, bottom, top
 fiextent = [meshx["min"],meshx["max"],meshvi["min"],meshvi["max"]] # left, right, bottom, top
 
@@ -197,25 +246,28 @@ def show_andor_save (fig, name):
             plt.close(fig)
         print("... Done saving %s." % title)
 
-if (plot_EE):
-    if (informations_on_graphs):
-        figE = plt.figure (figsize=(15,4))
-        [axE0,axET,axEInfos] = createAxesInfos (figE, Infos)
-    else:
-        figE, (axE0, axET) = plt.subplots (1,2,sharex=True,sharey=True,figsize=(11,4))
+# functions from (t,x) -> IR (E, rho)
+for (plot, ffs, col, name, tag, minn, maxx) in zip ([plot_EE, plot_rho], [Efields,rhos],["steelblue","coral"],["Electric field","Rho"],["E","rho"],[Emin,rhomin],[Emax,rhomax]):
+    if (plot):
+        if (informations_on_graphs):
+            fig = plt.figure (figsize=(15,4))
+            [ax0,axT,axInfos] = createAxesInfos (fig, Infos)
+        else:
+            fig, (ax0, axT) = plt.subplots (1,2,sharex=True,sharey=True,figsize=(11,4))
 
-    for iax, ax, tt, Efield in zip([0,1],[axE0,axET], [times[0],times[-1]], [Efields[0],Efields[-1]]):
-        ax.plot(xxE, Efield, marker=".", color="steelblue")
-        ax.set_title("Time t=%6.3f" % tt)
-        ax.set_xlabel("x")
-        if (iax==0):
-            ax.set_ylabel("E")
-        ax.set_ylim([Emin-0.05*(Emax-Emin),Emax+0.01*(Emax-Emin)])
+        for iax, ax, tt, ff in zip([0,1],[ax0,axT], [times[0],times[-1]], [ffs[0],ffs[-1]]):
+            ax.plot(xx, ff, marker=".", linestyle="none", markersize=4, color=col)
+            ax.set_title("Time t=%6.3f" % tt)
+            ax.set_xlabel("x")
+            if (iax==0):
+                ax.set_ylabel(tag)
+            ax.set_ylim([minn-0.05*(maxx-minn),maxx+0.01*(maxx-minn)])
 
-    figE.suptitle("Electric field at initial and final time")
-    show_andor_save (figE, "E")
+        fig.suptitle("%s at initial and final time" % (name))
+        show_andor_save (fig, tag)
 
-for (plot,ffs,extent,species,maxx) in zip([plot_fi, plot_fe],[fis,fes],[fiextent,feextent],["Ion", "Electron"],[maxi,maxe]):
+# functions from (t,x,v) -> IR (fe, fi)
+for (plot,ffs,extent,species,minn,maxx) in zip([plot_fi, plot_fe],[fis,fes],[fiextent,feextent],["Ion", "Electron"],[mini,mine],[maxi,maxe]):
     if (plot):
         if (informations_on_graphs):
             fig = plt.figure (figsize=(15,4))
@@ -224,7 +276,7 @@ for (plot,ffs,extent,species,maxx) in zip([plot_fi, plot_fe],[fis,fes],[fiextent
             fig, (ax0, axT) = plt.subplots (1,2,sharex=True,sharey=True,figsize=(11,4))
 
         for iax, ax, ff in zip([0,1], [ax0, axT], [ffs[0],ffs[-1]]):
-            im = ax.imshow (ff,vmin=0.0,vmax=maxx,cmap='jet',origin='lower',extent=extent,aspect="auto",interpolation='spline36')
+            im = ax.imshow (ff,cmap=cmap,vmin=minn,vmax=maxx,origin='lower',extent=extent,aspect="auto",interpolation='spline36')
             ax.set_xlabel("x")
             if (iax==0):
                 ax.set_ylabel("v")
@@ -241,33 +293,35 @@ for (plot,ffs,extent,species,maxx) in zip([plot_fi, plot_fe],[fis,fes],[fiextent
 
 # Source : https://stackoverflow.com/questions/34975972/how-can-i-make-a-video-from-array-of-images-in-matplotlib
 
-factor = 1.0 # 1.0 = real time, less = slow motion, more = accelerated
 video_length = factor * dt * num_iteration * 1000 # in seconds
 
-if (video_EE):
-    name = saverep_mp4+"E.mp4"
-    print("Creating video %s... (please wait, it can take a few seconds)" % name)
-    if (informations_on_graphs):
-        figvE, (axvE, axvEInfos) = plt.subplots (1,2,figsize=(11,4))
-        writeInfos (axvEInfos, Infos)
-    else:
-        figvE, axvE = plt.subplots (figsize=(8,4))
-    axvE.set_xlabel("x")
-    axvE.set_ylabel("E")
+# functions from (t,x) -> IR (E, rho)
+for (vid,tag,its,ffs,minn,maxx,col) in zip([video_EE,video_rho],["E","rho"],[E_its,rho_its],[Efields,rhos],[Emin,rhomin],[Emax,rhomax],["steelblue","coral"]):
+    if (vid):
+        name = saverep_mp4+tag+".mp4"
+        print("Creating video %s... (please wait, it can take a few seconds)" % name)
+        if (informations_on_graphs):
+            figv, (axv, axvInfos) = plt.subplots (1,2,figsize=(11,4))
+            writeInfos (axvInfos, Infos)
+        else:
+            figv, axv = plt.subplots (figsize=(8,4))
+        axv.set_xlabel("x")
+        axv.set_ylabel(tag)
 
-    frames = []
-    for tt, it, E in zip(times, E_its, Efields):
-        label = "t = %6.3f / %6.3f, it %d / %d" % (tt, dt*num_iteration, it, num_iteration)
-        txt = axvE.text(0.0,-0.1*Emin+1.1*Emax,label,fontsize=14,ha="center",va="bottom")
-        im, = axvE.plot (xxE, E, marker=".", color="steelblue", animated=True)
-        frames.append([im, txt])
+        frames = []
+        for tt, it, ff in zip(times, its, ffs):
+            label = "t = %6.3f / %6.3f, it %d / %d" % (tt, dt*num_iteration, it, num_iteration)
+            txt = axv.text(0.0,-0.1*minn+1.1*maxx,label,fontsize=14,ha="center",va="bottom")
+            im, = axv.plot (xx, ff, marker=".", color=col,linestyle="none", markersize=4, animated=True)
+            frames.append([im, txt])
 
-    videoE = animation.ArtistAnimation (figvE, frames, interval=video_length / len(frames), repeat=False)
-    videoE.save (name)
-    print("...Done creating video %s." % name)
-    # plt.show ()
+        video = animation.ArtistAnimation (figv, frames, interval=video_length / len(frames), repeat=False)
+        video.save (name)
+        print("...Done creating video %s." % name)
+        # plt.show ()
 
-for (vid,its,ffs,meshv,extent,tag,maxx) in zip([video_fe,video_fi],[fe_its,fi_its],[fes,fis],[meshve,meshvi],[feextent,fiextent],["fe","fi"],[maxe,maxi]):
+# functions from (t,x,v) -> IR (fe, fi)
+for (vid,its,ffs,meshv,extent,tag,minn,maxx) in zip([video_fe,video_fi],[fe_its,fi_its],[fes,fis],[meshve,meshvi],[feextent,fiextent],["fe","fi"],[mine,mini],[maxe,maxi]):
     if (vid):
         name = saverep_mp4+tag+".mp4"
         print("Creating video %s... (please wait, it can take a few seconds)" % name)
@@ -283,7 +337,7 @@ for (vid,its,ffs,meshv,extent,tag,maxx) in zip([video_fe,video_fi],[fe_its,fi_it
         for tt, it, ff in zip(times, its, ffs):
             label = "t = %6.3f / %6.3f, it %d / %d" % (tt, dt*num_iteration, it, num_iteration)
             txt = axv.text(0.0,-0.01*meshv["min"]+1.01*meshv["max"],label,fontsize=14,ha="center",va="bottom")
-            im = axv.imshow (ff,vmin=0.0,vmax=maxx,cmap='jet',origin='lower',extent=extent,aspect="auto",interpolation='spline36', animated=True)
+            im = axv.imshow (ff,cmap=cmap,vmin=minn,vmax=maxx,origin='lower',extent=extent,aspect="auto",interpolation='spline36', animated=True)
             if (it==its[0]):
                 # source : https://stackoverflow.com/questions/13310594/positioning-the-colorbar
                 axins = inset_axes(axv, width="5%", height="100%", loc='center left', borderpad=-10)
